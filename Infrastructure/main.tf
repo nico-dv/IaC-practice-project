a22a56dc333abdc9f8e27996955c6cb530ac3d8a
nico-dv <vv.nicov@gmail.com>
2022-07-02 15:57:08 +0100

    added two instances

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,35 +1,38 @@
-resource "aws_instance" "server" {
+resource "aws_instance" "private_server" {
 
-  instance_type          = "t2.micro"
-  ami                    = data.aws_ami.server_ami.id
-  subnet_id              = aws_subnet.private.id
-
-
-  root_block_device {
-    volume_size = 10
-  }
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.server_ami.id
+  subnet_id     = aws_subnet.private.id
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
 
   tags = {
-    Name = "Server"
+    Name = "Private"
   }
 
 }
 
 
 
-resource "aws_instance" "client" {
-  
+resource "aws_instance" "public_server" {
+
   instance_type          = "t2.micro"
   ami                    = data.aws_ami.server_ami.id
+  user_data              = file("userdata.tpl")
+  key_name               = aws_key_pair.main_key.id
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
   subnet_id              = aws_subnet.public.id
 
-
-  root_block_device {
-    volume_size = 10
-  }
-
   tags = {
-    Name = "Client"
+    Name = "Public"
   }
+
+
+}
+
+
+
+resource "aws_key_pair" "main_key" {
+  key_name   = "devops-project1"
+  public_key = file("~/.ssh/devops-project1.pub")
 
 }

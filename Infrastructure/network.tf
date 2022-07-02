27ea142b959b72330013c60b4744584c669ec08b
nico-dv <vv.nicov@gmail.com>
2022-07-02 18:12:31 +0100

    finished network.tf

--- Infrastructure/network.tf
+++ Infrastructure/network.tf
@@ -90,7 +90,7 @@ resource "aws_security_group" "private_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.server_ip}"]
   }
 
   egress {
